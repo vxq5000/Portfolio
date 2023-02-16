e7d00a9e497f181f41830e75ba97236e99556fac
Venus Quan <vxq5000@gmail.com>
2023-02-15 16:57:52 -0800

    started projects page and added video

--- server.py
+++ server.py
@@ -12,5 +12,10 @@ def resume():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__=="__main__":
     app.run(debug=True)
