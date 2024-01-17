57ba6c4a34078033b30d71a215726effb1889eeb
Venus Quan <94498212+vxq5000@users.noreply.github.com>
2024-01-16 16:48:54 -0800

    edit route

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('mainpage.html')
+    return render_template('index.html')
 
 @app.route('/resume')
 def resume():
