78efd3c2b6bad659b775cf242123de42e7f82982
Venus Quan <94498212+vxq5000@users.noreply.github.com>
2024-02-16 14:07:24 -0800

    added links to a tags

---
+++ flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
